e3f7f40b21c26bb8e5f0c966e86289c4b0296037
Jaroslav <xkvasn14@vutbr.cz>
2021-10-12 19:11:14 +0200

    argument parser

--- c.cpp
+++ c.cpp
@@ -1,7 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pcap.h>
+#include <iostream>
+#include <string.h>
+#include <filesystem>
 
-int main ()
+
+int argument_parser(int argc, char** argv, char *ipaddress, char *rfile)
 {
-    printf("Hello World");
-    return 1;
+    int opt;
+    bool isserver = false;
+    bool isrFile = false;
+    bool isIpaddress = false;
+
+    while((opt = getopt(argc,argv,"-r:-i:-I")) != -1)
+    {
+        switch (opt)
+        {
+            /*
+            case 'h':
+             call help;
+             break;
+             * */
+            case 'r':
+                strcpy(rfile, optarg);
+                isrFile = true;
+                break;
+            case 'i':
+                strcpy(ipaddress, optarg);
+                isIpaddress = true;
+                break;
+            case 'I':
+                isserver = true;
+                break;
+            default:
+                printf("Wrong arguments");
+                exit(1);
+                break;
+        }
+        printf("Isserver: %d\n",isserver);
+    }
+    printf("Isserver: %d\n",isserver);
+    if (isserver)
+        return 1;
+    if(isrFile && isIpaddress)
+        return 0;
+    return -1;
+}
+
+
+int main (int argc, char *argv[])
+{
+    char *ipaddress = (char*)malloc(50*sizeof(char));
+    char *rfile = (char*)malloc(50*sizeof(char));
+
+    int isserver = argument_parser(argc,argv,ipaddress,rfile);
+    if(isserver == -1)
+    {
+        printf("Some arguments are missing\n");
+        free(ipaddress);
+        free(rfile);
+    }
+    if(isserver == 1)
+    {
+        // CALL SERVER BRANCH
+    }
+    else
+    {
+        // CALL CLIENT BRANCH
+    }
+    return 0;
 }
